afdf602c6aacca805fb48523dada22257841f0e1
Mesut Gunes <mesut.gunes3401@gmail.com>
2022-03-26 19:57:14 +0000

    rectify

--- src/day01gitgithub/FirstJavaClass.java
+++ src/day01gitgithub/FirstJavaClass.java
@@ -4,6 +4,6 @@ public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
         System.out.println("Adding a new code");
-        System.out.println("This an other code");
+        System.out.println("This is an other code");
     }
 }
